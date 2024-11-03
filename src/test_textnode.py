9b7918fecf5e2cc7020e549b496149af15325797
hugoivankm <hugoivank@gmail.com>
2024-11-02 19:27:32 -0600

    Add additional tests for edge cases and exceptions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,18 @@
 import unittest
+from enum import Enum
 
-from textnode import TextNode, TextType
+from textnode import TextNode
+
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+    ERROR = "error"
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -15,7 +27,6 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is italic", TextType.ITALIC)
         node2 = TextNode("This is italic", TextType.ITALIC)
         self.assertEqual(node, node2)
-        
                 
         node = TextNode("This is code", TextType.CODE)
         node2 = TextNode("This is code", TextType.CODE)
@@ -24,6 +35,19 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("image", TextType.IMAGE, "https://picsum.photos/200/300" )
         node2 = TextNode("image", TextType.IMAGE, "https://picsum.photos/200/300" )
         self.assertEqual(node, node2)
+    
+    def test_not_eq(self):
+        node = TextNode("This is a bol", TextType.BOLD)
+        node2 = TextNode("This is a bold", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+        
+        image1 = TextNode("image", TextType.IMAGE, None)
+        image2 = TextNode("image", TextType.IMAGE)
+        self.assertNotEqual(image1.__repr__(), image2.__repr__())
+    
+    def test_non_existent_node_raises_exception(self):    
+        self.assertRaises(ValueError, TextNode, "This is a text", TextType.ERROR)
+        
         
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,10 +9,19 @@ class TextType(Enum):
     IMAGE = "image"
     
 class TextNode:
-    def __init__(self, text, text_type, url = None) -> None:
+    '''
+    Text Node class includes methods and classes to work with text nodes for HTML
+    '''
+    __Sentinel = object()
+    def __init__(self, text, text_type, url=__Sentinel) -> None:
         self.text = text
-        self.text_type = text_type.value
+        self.text_type = text_type
         self.url = url
+        
+        valid_text_types = TextNode.__get_valid_text_nodes_type()
+        
+        if not hasattr(self.text_type, 'value') or (self.text_type.value not in valid_text_types):
+             raise ValueError("type not in text types")                
     
     def __eq__(self, other: object) -> bool:
         return (
@@ -22,9 +31,18 @@ class TextNode:
         )
     
     def __repr__(self):
+        if self.url == TextNode.__Sentinel:
+            return f"TextNode({self.text}, {self.text_type.value})"            
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
     @classmethod
     def print_text_types(cls):
         for type in TextType:
             print(type.value)
+    
+    @classmethod
+    def __get_valid_text_nodes_type(cls):
+        valid_text_nodes_type = []
+        for type in TextType:
+            valid_text_nodes_type.append(type.value)
+        return valid_text_nodes_type
