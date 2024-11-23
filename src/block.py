ebde1ae72b6c2a75a0b833a0f46d40aece0397d5
hugoivankm <hugoivank@gmail.com>
2024-11-23 15:09:44 -0600

    Add block to block type functionality and testing

--- src/block.py
+++ src/block.py
@@ -1,14 +1,15 @@
 from enum import Enum
 import re
 
+
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
     CODE = "code"
     QUOTE = "quote"
-    UNORDERED_LIST = "unordered_list"
-    ORDEREDLIST = "ordered_list"
-    
+    UNORDERED_LIST = "unordered list"
+    ORDEREDLIST = "ordered list"
+
 
 class Block:
     """
@@ -18,25 +19,42 @@ class Block:
         content - Markdown content for the block
         block_type - The type of content this block node contains, which is a member of the Block enum.
     """
+
     def __init__(self, content: str, block_type: BlockType) -> None:
         if not content:
             raise ValueError("Block node must have markdown content")
-        
+
         self.content = content
         self.block_type = block_type
-        
+
     def __repr__(self) -> str:
         return f'Block("{self.content}", "{self.block_type}")'
-    
+
     def __eq__(self, other):
         return self.content == other.content and self.block_type == other.block_type
-    
 
-        
-        
-        
+    @staticmethod
+    def block_to_block_type(markdown_block):
+        block_re_dict = {
+            BlockType.HEADING: (r"^#{1,6} (.+)", 'single'),
+            BlockType.CODE: (r"^```\n?.+\n?```$", 'single'),
+            BlockType.QUOTE: (r"^>(.*?)", 'single'),
+            BlockType.UNORDERED_LIST: (r"^(\*|-) (.*)$", 'multi'),
+            BlockType.ORDEREDLIST: (r"^\d+.\s(.+)$", 'multi'),
+        }
 
-        
-        
-        
-        
+        # pattern is a BlockTYpe
+        for pattern in block_re_dict:
+            regex = block_re_dict[pattern][0]
+            multiline = block_re_dict[pattern][1]
+
+            if Block._matches_pattern(regex, markdown_block, multiline):
+                return pattern.value
+        return BlockType.PARAGRAPH.value
+
+    @staticmethod
+    def _matches_pattern(pattern, markdown, multiline):
+        if multiline == 'multi':
+            return bool(re.match(pattern, markdown, re.MULTILINE))
+        else:
+            return bool(re.match(pattern, markdown))
--- tests/test_block.py
+++ tests/test_block.py
@@ -1,19 +1,45 @@
 import unittest
 from src.block import Block, BlockType
 
+
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node1 = Block("This is a paragraph", BlockType.PARAGRAPH)
         node2 = Block("This is a paragraph", BlockType.PARAGRAPH)
         self.assertEqual(node1, node2)
-        
+
         node1 = Block("###This is a heading", BlockType.HEADING)
         node2 = Block("###This is a heading", BlockType.HEADING)
-        
+
     def test_not_eq(self):
-        node1 = Block("* This is the first list item in a list block\n* This is a list item\n* This is another list item", BlockType.UNORDERED_LIST)
-        node2 = Block("* This is the first list item in a list block\n This is a list item\n*This is another list item", BlockType.UNORDERED_LIST)
+        node1 = Block(
+            "* This is the first list item in a list block\n* This is a list item\n* This is another list item", BlockType.UNORDERED_LIST)
+        node2 = Block(
+            "* This is the first list item in a list block\n This is a list item\n*This is another list item", BlockType.UNORDERED_LIST)
         self.assertNotEqual(node1, node2)
-        
+
     def test_repr__(self):
-        return (Block("> This is a quote", BlockType.QUOTE).__repr__ == 'Block("{> This is a quote"}", "{BlockType.QUOTE}")')
+        self.assertEqual(Block("> This is a quote", BlockType.QUOTE).__repr__(
+        ), 'Block("> This is a quote", "BlockType.QUOTE")')
+
+    def test_block_to_block_type(self):
+        heading_text = "# This is a heading"
+        actual = Block.block_to_block_type(heading_text)
+        expected = "heading"
+        self.assertEqual(actual, expected)
+
+        paragraph_text = "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
+        actual = Block.block_to_block_type(paragraph_text)
+        expected = "paragraph"
+        self.assertEqual(actual, expected)
+
+        ordered_list = "1. First Item\n2. Second Item\n3. Third Item"
+        actual = Block.block_to_block_type(ordered_list)
+        expected = "ordered list"
+        self.assertEqual(actual, expected)
+        
+        
+        unordered_list = "* First Item\n* Second Item\n* Third Item"
+        actual = Block.block_to_block_type(unordered_list)
+        expected = "unordered list"
+        self.assertEqual(actual, expected)
